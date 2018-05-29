23eb3fd83e64619173ad1ddee2781be2030a22fa
fluffy <fluffy@beesbuzz.biz>
2018-05-29 16:06:56 -0700

    nwo working in flask

--- flrig.py
+++ flrig.py
@@ -1,13 +1,25 @@
 import flask
 import feedparser
 import requests
+import re
 
 app = flask.Flask(__name__)
 
 
-@app.route('/')
-@app.route('/<string:tag>')
-def flrig(tag=None):
+def print_license(url):
+    cc = re.search(r'licenses/(.*)/deed', url)
+    if cc:
+        return flask.Markup('''
+<a href="{url}"><img src="https://licensebuttons.net/l/{license}/88x31.png" title="Creative Commons {license}" alt="{license}"> Creative Commons {license}</a>
+'''.format(url=url, license=cc.group(1)))
+
+    if 'publicdomain' in url:
+        return flask.Markup('''<a href="{url}">Public domain</a>'''.format(url=url))
+
+    return flask.Markup('<a href="{url}">unrecognized license</a>'.format(url=url))
+
+
+def get_feed(tag=None):
     params = {'format': 'atom'}
     if tag:
         params['tag'] = tag
@@ -16,7 +28,13 @@ def flrig(tag=None):
         'http://api.flickr.com/services/feeds/photos_public.gne', params=params)
     feed = feedparser.parse(req.text)
 
-    return flask.render_template('flrig.html', feed=feed, tag=tag)
+    return feed
+
+
+@app.route('/')
+@app.route('/<string:tag>')
+def flrig(tag=None):
+    return flask.render_template('flrig.html', feed=get_feed(tag), tag=tag, print_license=print_license)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
