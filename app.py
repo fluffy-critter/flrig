3bf7c82d99a6ece5fc0694403530c950632eda45
fluffy <fluffy@beesbuzz.biz>
2025-01-09 17:23:58 -0800

    Add ProxyFIx wrapper for hypercorn

--- app.py
+++ app.py
@@ -97,6 +97,8 @@ def robots_txt():
     """ robots.txt handler """
     return flask.send_file('robots.txt')
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 if __name__ == '__main__':
     app.run(debug=True)
