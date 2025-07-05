e489be77250b5f68c032d797ef694b38d7a154f0
fluffy <fluffy@beesbuzz.biz>
2025-07-05 13:18:04 -0700

    Add basic anti-AI-bot stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """ Flickr Random Image Generatr """
 
+from werkzeug.middleware.proxy_fix import ProxyFix
 import feedparser
 import flask
 import requests
@@ -12,24 +13,28 @@ import logging.handlers
 import os
 import markupsafe
 import user_agents
+import uuid
 
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
 logging.basicConfig(level=logging.INFO,
                     handlers=[
                         logging.handlers.TimedRotatingFileHandler(
-                            os.path.join(APP_PATH,'logs','flrig.log'), when='D'),
+                            os.path.join(APP_PATH, 'logs', 'flrig.log'), when='D'),
                         logging.StreamHandler()
                     ],
                     format="%(levelname)s:%(threadName)s:%(name)s:%(message)s")
 
-LOGGER=logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 app = flask.Flask(__name__)
+app.secret_key = str(uuid.uuid4())
+
 cache = Cache(app, config={
     'CACHE_TYPE': 'memcached',
     'CACHE_DEFAULT_TIMEOUT': 60,
     'CACHE_KEY_PREFIX': 'flrig.beesbuzz.biz',
 })
+
 
 @cache.cached()
 def get_feed(tag=None):
@@ -74,8 +79,11 @@ def flrig(tag=None):
     if tag and wordfilter.Wordfilter().blacklisted(tag):
         raise http_error.NotFound("I don't know what that word means")
 
-    # temporary (hopefully) mitigation due to LLM bot flood
-    # tag = None
+    is_bot = user_agents.parse(flask.request.headers.get('User-Agent')).is_bot
+
+    # if there's a tag and no valid session ID, send over to the pseudo-captcha
+    if tag and not is_bot and not flask.session.get('sid'):
+        return flask.render_template('gatekeep.html', sid=str(uuid.uuid4())), 401
 
     try:
         return flask.render_template(
@@ -83,19 +91,31 @@ def flrig(tag=None):
             feed=get_feed(tag),
             tag=tag,
             filter_description=filter_description,
-            is_bot = user_agents.parse(flask.request.headers.get('User-Agent')).is_bot)
+            is_bot=is_bot)
     except requests.exceptions.RequestException as ex:
         LOGGER.warning("Upstream error for tag %s", tag)
         return flask.render_template(
             'error.html',
             error=ex)
 
+
+@app.route('/', methods=["POST"])
+@app.route('/<string:tag>', methods=["POST"])
+def gatekeep(tag=None):
+    """ handle potential AI bots """
+    if not flask.request.form.get('sid'):
+        raise http_error.BadRequest("missing SID")
+
+    flask.session['sid'] = flask.request.form['sid']
+    return flask.redirect(flask.url_for('flrig', tag=tag))
+
+
 @app.route('/robots.txt')
 def robots_txt():
     """ robots.txt handler """
     return flask.send_file('robots.txt')
 
-from werkzeug.middleware.proxy_fix import ProxyFix
+
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 if __name__ == '__main__':
