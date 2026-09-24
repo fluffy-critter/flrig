47572471d3be646f9da1e1346c3a2ee9d87c1b7f
fluffy <fluffy@beesbuzz.biz>
2026-09-24 00:33:51 -0700

    Improve antibot

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ import os
 import re
 import uuid
 
+import arrow
 import feedparser
 import flask
 import markupsafe
 import requests
 import requests.exceptions
 import user_agents
-import werkzeug.exceptions as http_error
+import werkzeug.exceptions
 import wordfilter
 from flask_caching import Cache
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
                     handlers=[
                         logging.handlers.TimedRotatingFileHandler(
                             os.path.join(APP_PATH, 'logs', 'flrig.log'), when='D'),
@@ -73,46 +74,92 @@ def filter_description(content):
     return markupsafe.Markup('\n'.join(lines))
 
 
+def keymaster(sid):
+    """ Generates a salted token for the browser """
+    import hashlib
+
+    parts = [
+        str(sid),
+        flask.request.remote_addr,
+        flask.request.headers.get('User-Agent')
+    ]
+    token = hashlib.md5('|'.join(parts).encode('utf-8')) # ty:ignore[no-matching-overload]
+
+    return token.digest()
+
 @app.before_request
 def is_bot():
+    """ simple bot detection """
     ua_string = flask.request.headers.get('User-Agent')
-    flask.g.is_bot = (re.search(r'\+https?://', ua_string) or
-                      user_agents.parse(ua_string).is_bot)
+    flask.g.is_bot = ('+' in ua_string or
+                      user_agents.parse(ua_string).is_bot or
+                      'referer' not in flask.request.headers)
 
+@app.errorhandler(werkzeug.exceptions.TooManyRequests)
+def antibot(e):
+    response = e.get_response()
+    response.data = flask.render_template('gatekeep.html', sid=arrow.now().format('X'))
+    return response
+
+@app.errorhandler(werkzeug.exceptions.HTTPException)
+def error_page(e):
+    response = e.get_response()
+    response.data = flask.render_template('error.html', error=e)
+    return response
 
 @app.route('/')
 @app.route('/<string:tag>')
 def flrig(tag=None):
     """ main page handler """
     LOGGER.debug("root %s", tag)
+
+    if '&amp;' in flask.request.url:
+        raise werkzeug.exceptions.BadRequest("learn how HTML entities work, you stupid bot")
+
+
+    if tag:
+        # check for a passed sentience check
+        LOGGER.debug("checking sentience")
+        try:
+            sid, token = flask.session['sid']
+            now = arrow.utcnow()
+            LOGGER.debug("now=%s sid=%s", now, arrow.get(sid))
+            LOGGER.debug("token=%s key=%s", token, keymaster(sid))
+            if not (now.shift(hours=-1) < arrow.get(float(sid)) < now and
+                keymaster(sid) == token):
+                LOGGER.debug("session invalid")
+                raise werkzeug.exceptions.TooManyRequests(retry_after=3600)
+        except (KeyError, ValueError, arrow.ParserError) as e:
+            LOGGER.debug("missing or malformed session: %s", e)
+            raise werkzeug.exceptions.TooManyRequests(retry_after=3600)
+
     if tag and wordfilter.Wordfilter().blacklisted(tag):
-        raise http_error.NotFound("I don't know what that word means")
+        raise werkzeug.exceptions.NotFound("I don't know what that word means")
 
-    # if there's a tag and no valid session ID, send over to the pseudo-captcha
-    if tag and not flask.g.is_bot and not flask.session.get('sid'):
-        return flask.render_template('gatekeep.html', sid=str(uuid.uuid4())), 429
-
-    try:
-        return flask.render_template(
-            'flrig.html',
-            feed=get_feed(tag),
-            tag=tag,
-            filter_description=filter_description)
-    except requests.exceptions.RequestException as ex:
-        LOGGER.warning("Upstream error for tag %s", tag)
-        return flask.render_template(
-            'error.html',
-            error=ex)
+    return flask.render_template(
+        'flrig.html',
+        feed=get_feed(tag),
+        tag=tag,
+        filter_description=filter_description)
 
 
 @app.route('/', methods=["POST"])
 @app.route('/<string:tag>', methods=["POST"])
 def gatekeep(tag=None):
     """ handle potential AI bots """
-    if not flask.request.form.get('sid'):
-        raise http_error.BadRequest("missing SID")
+    try:
+        sid = float(flask.request.form['sid'])
+    except ValueError:
+        raise werkzeug.exceptions.BadRequest("invalid token")
 
-    flask.session['sid'] = flask.request.form['sid']
+    now = arrow.now()
+
+    if arrow.get(sid) > now:
+        raise werkzeug.exceptions.BadRequest("hello time traveler")
+    if arrow.get(sid) < now.shift(minutes=-15):
+        raise werkzeug.exceptions.TooManyRequests(retry_after=3600)
+
+    flask.session['sid'] = (sid, keymaster(sid))
     return flask.redirect(flask.url_for('flrig', tag=tag))
 
 
